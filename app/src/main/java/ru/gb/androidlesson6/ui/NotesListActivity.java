20d3d4d3d68cbc6645a03d044d64ab5ff5277826
neqelovek <neqelovek@gmail.com>
2022-01-06 19:06:03 +0300

    Lesson6

--- app/src/main/java/ru/gb/androidlesson6/ui/NotesListActivity.java
+++ app/src/main/java/ru/gb/androidlesson6/ui/NotesListActivity.java
@@ -29,9 +29,6 @@ public class NotesListActivity extends AppCompatActivity implements NotesAdapter
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_list);
 
-
-        fillRepo();
-
         adapter = new NotesAdapter();
         adapter.setNotes(repository.getAll());
 
@@ -40,22 +37,6 @@ public class NotesListActivity extends AppCompatActivity implements NotesAdapter
         list = findViewById(R.id.list);
         list.setLayoutManager(new LinearLayoutManager(this));
         list.setAdapter(adapter);
-    }
-
-    private void fillRepo() {
-        repository.create(new Note("Title 1", "description 1"));
-        repository.create(new Note("Title 2", "description 2"));
-        repository.create(new Note("Title 3", "description 3"));
-        repository.create(new Note("Title 4", "description 4"));
-        repository.create(new Note("Title 5", "description 5"));
-        repository.create(new Note("Title 6", "description 6"));
-        repository.create(new Note("Title 7", "description 7"));
-        repository.create(new Note("Title 8", "description 8"));
-        repository.create(new Note("Title 9", "description 9"));
-        repository.create(new Note("Title 10", "description 10"));
-        repository.create(new Note("Title 11", "description 11"));
-        repository.create(new Note("Title 12", "description 12"));
-
     }
 
     @Override
@@ -77,7 +58,7 @@ public class NotesListActivity extends AppCompatActivity implements NotesAdapter
             Intent intent = new Intent(this, EditNoteActivity.class);
             int id = repository.getAll().size() + 1;
             Note note = new Note(id, "", "");
-            intent.putExtra(Constants.NOTE_NEW, note);
+            intent.putExtra(Constants.NOTE, note);
             repository.create(note);
             startActivity(intent);
             return true;
